b8a4448c3776051102dc1b5763bb3fa461e242e8
Viktor Sharha <vsharha07@gmail.com>
2025-09-25 17:57:58 +0100

    added datasets, added api endpoint to get top n words

--- api.py
+++ api.py
@@ -5,26 +5,26 @@ import random
 
 app = FastAPI()
 
-class PartsOfSpeech(Enum):
+class PartOfSpeech(Enum):
     noun = "N"
     verb = "V"
     adjective = "ADJ"
 
-def parse_pos(pos: str = Query("n")):
-    for member in PartsOfSpeech:
+def parse_pos(pos: str = Query("n")) -> None | PartOfSpeech:
+    for member in PartOfSpeech:
         if pos.lower() == member.name.lower() or member.name.lower().startswith(pos.lower()):
             return member
     raise HTTPException(status_code=400, detail="Invalid part of speech")
 
-@app.get("/")
-def get_random_word(max_index: int | None = None, max_len: int | None = None, min_len: int | None = None, lang:str = "eng", pos: PartsOfSpeech = Depends(parse_pos)):
+def validate_dataset(lang:str, pos:PartOfSpeech) -> None:
     if datasets_map.get(lang) is None:
         raise HTTPException(status_code=400, detail="Language is currently not supported")
 
     if datasets_map.get(lang).get(pos.value) is None:
         raise HTTPException(status_code=400, detail=f"Part of speech {pos.value} is currently not supported for language {lang}")
 
-    words = list(datasets_map.get(lang).get(pos.value).keys())
+def filter_words(lang:str = "eng", pos:PartOfSpeech = Depends(parse_pos), max_index: int | None = None, max_len: int | None = None, min_len: int | None = None) -> list[str]:
+    words: list[str] = list(datasets_map.get(lang).get(pos.value).keys())
 
     if max_index is not None:
         words = [word for i, word in enumerate(words) if i < max_index]
@@ -35,7 +35,22 @@ def get_random_word(max_index: int | None = None, max_len: int | None = None, mi
     if max_len is not None:
         words = [word for word in words if len(word) <= max_len]
 
+    return words
+
+@app.get("/")
+def get_random_word(max_index: int | None = None, max_len: int | None = None, min_len: int | None = None, lang:str = "eng", pos: PartOfSpeech = Depends(parse_pos)) -> str:
+    validate_dataset(lang, pos)
+
+    words: list[str] = filter_words(lang, pos, max_index, max_len, min_len)
+
     if not words:
         raise HTTPException(status_code=404, detail="No words found with the given criteria")
 
     return random.choice(words)
+
+@app.get("/words")
+def get_word_list(length: int | None = None, lang:str = "eng", pos: PartOfSpeech = Depends(parse_pos)) -> list[str]:
+    validate_dataset(lang, pos)
+
+    words: list[str] = filter_words(lang, pos, length)
+    return words
