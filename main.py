5c80f6a365d2c351e110cc4d50765424c43d318f
Viktor Sharha <vsharha07@gmail.com>
2025-10-05 14:46:47 +0100

    added middleware

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI, HTTPException, Depends, Query
 from enum import Enum
+
+from starlette.middleware.cors import CORSMiddleware
+
 from load_datasets import datasets_map
 import random
 from pathlib import Path
 
 app = FastAPI()
+
+origins = [
+    # "http://localhost:5173",
+    "https://wordle-international.netlify.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class PartOfSpeech(Enum):
     noun = "N"
@@ -24,25 +40,29 @@ def validate_dataset(lang:str, pos:PartOfSpeech) -> None:
     if datasets_map.get(lang).get(pos.value) is None:
         raise HTTPException(status_code=400, detail=f"Part of speech {pos.value} is currently not supported for language {lang}")
 
-def filter_words(lang:str = "eng", pos:PartOfSpeech = Depends(parse_pos), max_index: int | None = None, max_len: int | None = None, min_len: int | None = None) -> list[str]:
-    words: list[str] = list(datasets_map.get(lang).get(pos.value).keys())
+def filter_words(lang:str = "eng", min_freq: int | None = None, max_len: int | None = None, min_len: int | None = None, max_index:int | None=None, pos:PartOfSpeech=PartOfSpeech.noun) -> list[str]:
+    word_freqs = datasets_map.get(lang).get(pos.value)
+    words = list(word_freqs.items())
 
-    if max_index is not None:
-        words = [word for i, word in enumerate(words) if i < max_index]
+    if min_freq is not None:
+        words = [(word, freq) for word, freq in words if freq >= min_freq]
 
     if min_len is not None:
-        words = [word for word in words if len(word) >= min_len]
+        words = [(word, freq) for word, freq in words if len(word) >= min_len]
 
     if max_len is not None:
-        words = [word for word in words if len(word) <= max_len]
+        words = [(word, freq) for word, freq in words if len(word) <= max_len]
 
-    return words
+    if max_index is not None:
+        words = words[:max_index]
 
-@app.get("/")
-def get_random_word(max_index: int | None = None, max_len: int | None = None, min_len: int | None = None, lang:str = "eng", pos: PartOfSpeech = Depends(parse_pos)) -> dict[str, str]:
+    return [word for word, _ in words]
+
+@app.get("/word")
+def get_random_word(min_freq: int | None = None, max_len: int | None = None, min_len: int | None = None, lang:str = "eng", pos: PartOfSpeech = Depends(parse_pos)) -> dict[str, str]:
     validate_dataset(lang, pos)
 
-    words: list[str] = filter_words(lang, pos, max_index, max_len, min_len)
+    words: list[str] = filter_words(lang, min_freq, max_len, min_len, pos)
 
     if not words:
         raise HTTPException(status_code=404, detail="No words found with the given criteria")
@@ -50,12 +70,14 @@ def get_random_word(max_index: int | None = None, max_len: int | None = None, mi
     return {"word": random.choice(words)}
 
 @app.get("/words")
-def get_word_list(length: int | None = None, lang:str = "eng", pos: PartOfSpeech = Depends(parse_pos)) -> dict[str, list[str]]:
+def get_word_list(min_freq: int | None = None, max_len: int | None = None, min_len: int | None = None, lang:str = "eng", max_index: int | None = None, pos: PartOfSpeech = Depends(parse_pos)) -> dict[str, list[str]]:
     validate_dataset(lang, pos)
 
-    words: list[str] = filter_words(lang, pos, length)
+    words: list[str] = filter_words(lang, min_freq, max_len, min_len, max_index, pos)
     return {"words": words}
 
 @app.get("/languages")
-def get_lang_list() -> dict[str, list[str]]:
-    return {"languages": [str(lang.name) for lang in Path("datasets").iterdir()]}
+def get_lang_list(pos: PartOfSpeech = Depends(parse_pos)) -> dict[str, list[str]]:
+    languages = [str(lang.name) for lang in Path("datasets").iterdir()]
+
+    return {"languages": [lang for lang in languages if datasets_map.get(lang) is not None and datasets_map.get(lang).get(pos.value) is not None]}
